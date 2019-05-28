de4217b50c8d17b631536e4e12dcbeb2eb948b73
Nathaniel Cain <nathaniel2897@gmail.com>
2019-05-28 15:17:32 -0400

    created post request to backend

--- bikeshare-app-client/src/App.js
+++ bikeshare-app-client/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar'
 import NetworkContainer from './components/NetworkContainer'
 // import UserProfile from './components/UserProfile'
 const API = 'https://api.citybik.es/v2/networks'
+const bikeNetworkBackend = 'http://localhost:3000/bike_networks'
 
 class App extends React.Component {
 
@@ -25,7 +26,18 @@ class App extends React.Component {
   }
 
   onAddNetworkToProfile = (selectedNetwork) => {
-    console.log("clicked!", selectedNetwork)
+    fetch(bikeNetworkBackend, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Accepts": "application/json",
+			},
+			body: JSON.stringify(selectedNetwork)
+		})
+    .then(r => r.json())
+    .then(data => {
+      console.log(data)
+    })
   }
 
   render() {
